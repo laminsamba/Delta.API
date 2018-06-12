1a1ae056b7e4db2fbaf0823cc3254a688641c6e0
Lamin Samba <Lamin.Samba@ofgem.gov.uk>
2018-06-12 12:53:24 +0100

    LS : Add niew model

---
+++ Delta.API/Delta.BLL/ViewModels/PersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delta.BLL.ViewModels
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AddressId { get; set; }
+    }
+}
